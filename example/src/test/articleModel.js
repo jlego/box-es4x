d4cc22a7f6059a993c75ae4868d5cbe5dea29a88
jlego <273734893@qq.com>
2021-02-26 01:30:17 +0800

    update articleModel.js

--- example/src/test/articleModel.js
+++ example/src/test/articleModel.js
@@ -1,5 +1,5 @@
 const schema = require('box-es4x/schema');
-
+// 子结构
 const SubSchema = new schema({
   a: {
     type: "number",
@@ -8,14 +8,24 @@ const SubSchema = new schema({
     type: "number",
   },
 });
-
+// 主结构
 const MainSchema = new schema({
   title: {
+    // 数据类型(必须)
     type: "string",
+    // 默认值(可不写)
+    default: '',
+    // 必填验证(可不写)
+    required: true,
+    // 自定义验证(可不写)
+    validator(val){
+      if(!val) return '标题不能为空'
+      return null
+    }
   },
   list: {
-    type: 'array',
-    schema: SubSchema
+    type: 'array',  //array或object
+    schema: SubSchema  //可多层嵌套
   },
 });
 
